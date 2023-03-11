03a76235bb09feabcf23e9788f72416868d9d985
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-03-11 12:46:21 +0100

    Create showTab function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,41 @@
-import "./home";
+import navbar from "./navbar";
+import home from "./home";
 import "./index.css";
-import "./contact";
-import "./menu";
+
+function showTab(tab) {
+  switch (tab) {
+    case "Home":
+      console.log("switch home");
+      break;
+    case "Menu":
+      console.log("switch menu");
+      break;
+    case "Contact":
+      console.log("switch contact");
+      break;
+    default:
+      console.log("no switch");
+  }
+}
+
+function showHome() {
+  const element = home;
+
+  return element;
+}
+
+function showMenu() {
+  const element = navbar;
+  return element;
+}
+
+const content = document.getElementById("content");
+
+const buttons = document.querySelectorAll("button");
+console.log(buttons);
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    showTab(button.textContent);
+  });
+});
---
+++ src/menu.js
@@ -0,0 +1,7 @@
+import "./navbar";
+
+const headline = document.createElement("h1");
+headline.textContent = "Menu";
+
+const content = document.getElementById("content");
+content.appendChild(headline);
