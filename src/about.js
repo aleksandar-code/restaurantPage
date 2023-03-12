5a0f22df2af06a57350924978da685476fca177f
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-03-12 11:29:04 +0100

    Update

--- src/about.js
+++ src/about.js
@@ -7,20 +7,15 @@ const About = () => {
   const contactInfo = document.createElement("div");
   contactInfo.classList.add("card");
   contactInfo.innerHTML =
-    "<p><strong>Kogen miammiam</strong></p><p>Phone number: 05 05 05 05</p> <p>Email: contact@email </p>";
-
-  const contactInfo2 = document.createElement("div");
-  contactInfo2.classList.add("card");
-  contactInfo2.innerHTML =
-    "<p><strong>Kogen blablabla</strong></p><p>Phone number: 05 05 05 05</p> <p>Email: contact@email </p>";
+    "<div>Contact</div><p><strong>Kogen miammiam</strong></p><p>Phone number: 05 05 05 05</p> <p>Email: contact@email </p>";
 
   const locationInfo = document.createElement("div");
   locationInfo.classList.add("card");
   locationInfo.innerHTML =
-    "<p><strong>London, Big Ben</strong></p><p>Great Britain</p> <p>Europe</p>";
+    "<div>Location</div><p><strong>London, Big Ben</strong></p><p>Great Britain</p> <p>Europe</p>";
 
   const content = document.getElementById("content");
-  contact.append(headline, contactInfo, contactInfo2, locationInfo);
+  contact.append(headline, contactInfo, locationInfo);
   content.appendChild(contact);
 };
 export default About;
